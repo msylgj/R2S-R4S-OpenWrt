28ce1333193e122e26f30ec54c2d309c102edf1a
SiYao Mo <1388852+msylgj@users.noreply.github.com>
2022-03-05 18:24:02 +0800

    remove chmod 755

--- SCRIPTS/target_r2s.sh
+++ SCRIPTS/target_r2s.sh
@@ -9,5 +9,3 @@ sed -i '/ff150000.i2c/a\\tset_interface_core 8 "ff160000.i2c"' target/linux/rock
 wget -P target/linux/rockchip/armv8/base-files/etc/hotplug.d/iface/ https://github.com/QiuSimons/OpenWrt-Add/raw/master/12-disable-rk3328-eth-offloading
 # 交换 lan/wan 口
 sed -i "s,'eth1' 'eth0','eth0' 'eth1',g" target/linux/rockchip/armv8/base-files/etc/board.d/02_network
-
-chmod -R 755 ./
--- SCRIPTS/target_r4s.sh
+++ SCRIPTS/target_r4s.sh
@@ -5,5 +5,3 @@ clear
 sed -i 's,-mcpu=generic,-mcpu=cortex-a72.cortex-a53+crypto,g' include/target.mk
 #IRQ 调优
 sed -i '/set_interface_core 20 "eth1"/a\\tset_interface_core 2 "ff3c0000.i2c"' target/linux/rockchip/armv8/base-files/etc/hotplug.d/net/40-net-smp-affinity
-
-chmod -R 755 ./
