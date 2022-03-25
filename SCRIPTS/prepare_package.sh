6c0c30184bba67c70555cebefc0d1c3afa048e9c
ZiMing Mo <msylgj@immortalwrt.org>
2022-03-26 07:03:36 +0800

    update

--- SCRIPTS/prepare_package.sh
+++ SCRIPTS/prepare_package.sh
@@ -30,8 +30,6 @@ svn co https://github.com/vernesong/OpenClash/branches/dev/luci-app-openclash fe
 # ServerChan
 rm -rf feeds/luci/applications/luci-app-serverchan
 git clone -b master --depth 1 https://github.com/tty228/luci-app-serverchan.git feeds/luci/applications/luci-app-serverchan
-# 翻译及部分功能优化
-svn co https://github.com/msylgj/OpenWrt-Add/trunk/addition-trans-zh package/emortal/addition-trans-zh
 
 ### 最后的收尾工作 ###
 # Lets Fuck
@@ -43,6 +41,7 @@ cp -f ../SCRIPTS/fuck package/base-files/files/usr/bin/fuck
 sed -i "s/'%D %V %C'/'Built by OPoA($(date +%Y.%m.%d))@%D %V'/g" package/base-files/files/etc/openwrt_release
 sed -i "/DISTRIB_REVISION/d" package/base-files/files/etc/openwrt_release
 sed -i "/%D/a\ Built by OPoA($(date +%Y.%m.%d))" package/base-files/files/etc/banner
+sed -i "/openwrt_banner/d" package/emortal/default-settings/files/99-default-settings
 sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
 sed -i 's/1608/1800/g' feeds/luci/applications/luci-app-cpufreq/root/etc/uci-defaults/cpufreq
 sed -i 's/2016/2208/g' feeds/luci/applications/luci-app-cpufreq/root/etc/uci-defaults/cpufreq
