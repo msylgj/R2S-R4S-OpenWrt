be38637eb08eb36f7b7b5db04d371e2ae10ca0d2
ZiMing Mo <msylgj@immortalwrt.org>
2022-03-28 14:21:50 +0800

    cleanup

--- SCRIPTS/target_r2s.sh
+++ SCRIPTS/target_r2s.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
-clear
 
 #使用特定的优化
 sed -i 's,-mcpu=generic,-mcpu=cortex-a53+crypto,g' include/target.mk
-# 配置 IRQ 并默认关闭 eth0 offloading rx/rx
-sed -i '/set_interface_core 4 "eth1"/a\\tset_interface_core 1 "ff150000.i2c"' target/linux/rockchip/armv8/base-files/etc/hotplug.d/net/40-net-smp-affinity
-sed -i '/ff150000.i2c/a\\tset_interface_core 8 "ff160000.i2c"' target/linux/rockchip/armv8/base-files/etc/hotplug.d/net/40-net-smp-affinity
-wget -P target/linux/rockchip/armv8/base-files/etc/hotplug.d/iface/ https://github.com/QiuSimons/OpenWrt-Add/raw/master/12-disable-rk3328-eth-offloading
 # 交换 lan/wan 口
 sed -i "s,'eth1' 'eth0','eth0' 'eth1',g" target/linux/rockchip/armv8/base-files/etc/board.d/02_network
--- SCRIPTS/target_r4s.sh
+++ SCRIPTS/target_r4s.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-clear
 
 #使用特定的优化
 sed -i 's,-mcpu=generic,-mcpu=cortex-a72.cortex-a53+crypto,g' include/target.mk
-#IRQ 调优
-sed -i '/set_interface_core 20 "eth1"/a\\tset_interface_core 2 "ff3c0000.i2c"' target/linux/rockchip/armv8/base-files/etc/hotplug.d/net/40-net-smp-affinity
