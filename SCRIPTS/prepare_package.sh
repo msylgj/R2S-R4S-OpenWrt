f37c44d31966cff8b750a712b646050f7362f613
ZiMing Mo <msylgj@immortalwrt.org>
2022-04-29 15:24:33 +0800

    refresh

--- SCRIPTS/prepare_package.sh
+++ SCRIPTS/prepare_package.sh
@@ -11,11 +11,17 @@ sed -i 's/Os/O3 -funsafe-math-optimizations -funroll-loops -ffunction-sections -
 ### 必要的 Patches ###
 # fix firewall3 flock
 patch -p1 < ../PATCHES/001-fix-firewall3-flock.patch
+# BBRv2 kernel 5.15
+#wget -qO ./target/linux/generic/hack-5.15/693-Add_BBRv2_congestion_control_for_Linux_TCP.patch https://gitlab.com/sirlucjan/kernel-patches/-/raw/master/5.15/bbr2-patches/0001-bbr2-5.15-introduce-BBRv2.patch 
 
 ### 获取额外的 LuCI 应用、主题 ###
 # MOD Argon
 rm -rf feeds/luci/themes/luci-theme-argon
 git clone -b randomPic --depth 1 https://github.com/msylgj/luci-theme-argon.git feeds/luci/themes/luci-theme-argon
+# MOD TurboACC To Add BBRv2
+# pushd feeds/luci/applications/luci-app-turboacc
+# patch -p1 < ../../../../../PATCHES/002-mod-turboacc-switch-bbr-support-to-bbr2.patch
+# popd
 # DNSPod
 svn co https://github.com/msylgj/OpenWrt_luci-app/trunk/luci-app-tencentddns feeds/luci/applications/luci-app-tencentddns
 ln -sf ../../../feeds/luci/applications/luci-app-tencentddns ./package/feeds/luci/luci-app-tencentddns
